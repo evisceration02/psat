bf882f54a0e9086446c832fb95a2d4280449308d
Nielson Phu <nsnphu@gmail.com>
2016-04-22 23:13:28 -0400

    Hardcode data.

--- src/js/psat.js
+++ src/js/psat.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Layout from "./components/Layout";
+import Calculator from "./components/Calculator";
+import stateCutoffs from "./cutoffs";
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<Layout />, app);
+ReactDOM.render(<Calculator stateCutoffs={stateCutoffs} />, app);
